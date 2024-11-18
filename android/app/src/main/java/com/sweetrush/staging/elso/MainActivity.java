f7409820699ca61f918decda8fb0f9538fc6dfc4
Rafael Emmanuelli <rafael.emmanuelli@sweetrush.com>
2024-11-18 16:59:59 -0400

    android fix 4

--- android/app/src/main/java/com/sweetrush/staging/elso/MainActivity.java
+++ android/app/src/main/java/com/sweetrush/staging/elso/MainActivity.java
@@ -9,25 +9,16 @@ import com.microsoft.clarity.models.ApplicationFramework;
 import com.microsoft.clarity.models.LogLevel;
 import java.util.Arrays;
 import java.util.Collections;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class MainActivity extends BridgeActivity {
   @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Your custom code here
-        ClarityConfig config = new ClarityConfig(
-          "oztc10g5eg",
-          null, // Default user id
-          LogLevel.None,
-          false, // Disallow metered network usage
-          true, // Enable web view capturing
-          Collections.singletonList("*"), // Allowed domains
-          ApplicationFramework.Native,
-          Collections.emptyList(), // Allowed activities
-          Collections.emptyList(), // Disallowed activities (ignore activities)
-          false // Disable on low-end devices
-        );
+        ClarityConfig config = new ClarityConfig("oztc10g5eg");
 
         Clarity.initialize(getApplicationContext(), config);
     }
