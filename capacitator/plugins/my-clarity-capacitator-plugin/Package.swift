63c8f7883d4c6b6b1a956df69df0ef6c43b49058
Rafael Emmanuelli <rafael.emmanuelli@sweetrush.com>
2026-08-05 15:28:55 -0400

    iOS Update Build 333

--- capacitator/plugins/my-clarity-capacitator-plugin/Package.swift
+++ capacitator/plugins/my-clarity-capacitator-plugin/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "MyClarityCapacitatorPlugin",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "MyClarityCapacitatorPlugin",
